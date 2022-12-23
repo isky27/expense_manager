78597712b9dc6389415805a5bad3d23cec9b3c25
VinayKumar1801 <107485549+VinayKumar1801@users.noreply.github.com>
2022-12-23 09:26:49 +0530

    vinay

---
+++ client/context/ChatProvider.js
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+
+
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+    const [selectedChat, setSelectedChat] = useState();
+    const [user, setUser] = useState();
+    const [notification, setNotification] = useState([]);
+    const [chats, setChats] = useState();
+
+    const history = useRouter();
+
+    useEffect(() => {
+        const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        setUser(userInfo);
+
+        if (!userInfo) history.replace("/");
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [history]);
+
+    return (
+        <ChatContext.Provider
+            value={{
+                selectedChat,
+                setSelectedChat,
+                user,
+                setUser,
+                notification,
+                setNotification,
+                chats,
+                setChats,
+            }}
+        >
+            {children}
+        </ChatContext.Provider>
+    );
+};
+
+export const ChatState = () => {
+    return useContext(ChatContext);
+};
+
+export default ChatProvider;
--- client/pages/chats.js
+++ client/pages/chats.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
+import Chatbox from "../components/Chatbox";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import MyChats from "../components/MyChats";
 
-const chats = () => {
+import {ChatState} from "../context/ChatProvider"
+
+const Chatpage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
+
   return (
-    <div>
-      hiiiii
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default chats
+export default Chatpage;
